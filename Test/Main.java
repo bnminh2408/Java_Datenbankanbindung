abd12763c50d04ce08e2d10f6cd3bb8dcbb22ace
Bui <nbui@o365.comhard.de>
2023-02-15 11:12:47 +0100

    duplikat code

--- Test/Main.java
+++ Test/Main.java
@@ -9,6 +9,7 @@ import DataAccessObject.DAOMitarbeiter;
 import model.ExterneMitarbeiter;
 import model.Kunden;
 import model.Mitarbeiter;
+import input.UserInput;
 
 public class Main {
 
@@ -31,27 +32,22 @@ public class Main {
 		int input;
 		boolean weiterMachen = true;
 		// ab hier
+
+		UserInput userInput = new UserInput();
+		
 		while (weiterMachen) {
-			switch (input = Integer.parseInt(inputValues(
+			switch (input = Integer.parseInt(userInput.inputValues(
 					"Weche folgende Daten moechten sie bearbeiten:\n" + "(1) Mitarbeiter\n" + "(2) Externe Mitarbeiter\n"
 							+ "(3) Kunden\n" + "(4) Quit\n" + "Bitte nur Zahlen von 1 bis 4 eingeben: ",
 					sc))) {
 			case 1: {
 				switch (input = Integer
-						.parseInt(inputValues("Was moechten sie tun ?\n" + "(1) Insert\n" + "(2) Delete\n"
+						.parseInt(userInput.inputValues("Was moechten sie tun ?\n" + "(1) Insert\n" + "(2) Delete\n"
 								+ "(3) Select All\n" + "(4) Select by ID\n" + "Bitte nur Zahlen von 1 bis 4 eingeben: ", sc))) {
 				case 1: {
-					int id = Integer.parseInt(inputValues("Bitte ID eingeben: ", sc));
-					String vorname = inputValues("Bitte Vorname eingeben: ", sc);
-					String nachname = inputValues("Bitte Nachname eingeben: ", sc);
-					String strasse = inputValues("Bitte Strasse eingeben: ", sc);
-					String plz = inputValues("Bitte Plz eingeben: ", sc);
-					String ort = inputValues("Bitte Ort eingeben: ", sc);
-					String telefon = inputValues("Bitte Telefon eingeben: ", sc);
-					String email = inputValues("Bitte Email eingeben: ", sc);
+					mitarbeiter = userInput.setMitarbeiter(sc);
 
-					daoMitarbeiter.insert(
-							mitarbeiter = new Mitarbeiter(id, vorname, nachname, strasse, plz, ort, telefon, email));
+					daoMitarbeiter.insert( mitarbeiter);
 					System.out.println();
 					System.out.println("Sie haben folgenden Daten hizugefuegt: " + mitarbeiter);
 
@@ -59,7 +55,7 @@ public class Main {
 				}
 				case 2: {
 					mitarbeiter.setMitarbeiterNr(input = Integer
-							.parseInt(inputValues("Sie möchte Mitarbeiter mit welche ID loeschen:...", sc)));
+							.parseInt(userInput.inputValues("Sie möchte Mitarbeiter mit welche ID loeschen:...", sc)));
 					daoMitarbeiter.delete(mitarbeiter);
 					break;
 				}
@@ -71,7 +67,7 @@ public class Main {
 				}
 				case 4: {
 					mitarbeiter.setMitarbeiterNr(
-							input = Integer.parseInt(inputValues("Sie moechte Mitarbeiter mit ID:  ", sc)));
+							input = Integer.parseInt(userInput.inputValues("Sie moechte Mitarbeiter mit ID:  ", sc)));
 					Mitarbeiter findMitarbeiter = daoMitarbeiter.selectById(mitarbeiter);
 					System.out.println(findMitarbeiter);
 					break;
@@ -82,21 +78,11 @@ public class Main {
 			}
 			case 2: {
 				switch (input = Integer
-						.parseInt(inputValues("Was moechten sie tun ?\n" + "(1) Insert\n" + "(2) Delete\n"
+						.parseInt(userInput.inputValues("Was moechten sie tun ?\n" + "(1) Insert\n" + "(2) Delete\n"
 								+ "(3) Select All\n" + "(4) Select by ID\n" + "Bitte nur Zahlen eingeben: ", sc))) {
 				case 1: {
-					int id = Integer.parseInt(inputValues("Bitte ID eingeben: ", sc));
-					String vorname = inputValues("Bitte Vorname eingeben: ", sc);
-					String nachname = inputValues("Bitte Nachname eingeben: ", sc);
-					String strasse = inputValues("Bitte Strasse eingeben: ", sc);
-					String plz = inputValues("Bitte Plz eingeben: ", sc);
-					String ort = inputValues("Bitte Ort eingeben: ", sc);
-					String telefon = inputValues("Bitte Telefon eingeben: ", sc);
-					String email = inputValues("Bitte Email eingeben: ", sc);
-					String firma = inputValues("Bitte Firma angeben: ", sc);
+					externeMitarbeiter = userInput.setExterneMitarbeiter(sc);
 
-					daoExterneMitarbeiter.insert(externeMitarbeiter = new ExterneMitarbeiter(id, vorname, nachname,
-							strasse, plz, ort, telefon, email, firma));
 					System.out.println();
 					System.out.println("Sie haben folgenden Daten hizugefuegt: " + externeMitarbeiter);
 
@@ -104,7 +90,7 @@ public class Main {
 				}
 				case 2: {
 					externeMitarbeiter.setMitarbeiterNr(input = Integer
-							.parseInt(inputValues("Sie moechte externe Mitarbeiter mit welche ID loeschen:... ", sc)));
+							.parseInt(userInput.inputValues("Sie moechte externe Mitarbeiter mit welche ID loeschen:... ", sc)));
 					daoMitarbeiter.delete(externeMitarbeiter);
 					break;
 				}
@@ -117,7 +103,7 @@ public class Main {
 				case 4: {
 
 					externeMitarbeiter.setMitarbeiterNr(
-							input = Integer.parseInt(inputValues("Sie suchen externe Mitarbeiter mit ID:  ", sc)));
+							input = Integer.parseInt(userInput.inputValues("Sie suchen externe Mitarbeiter mit ID:  ", sc)));
 					ExterneMitarbeiter findMitarbeiter = daoExterneMitarbeiter.selectById(externeMitarbeiter);
 					System.out.println(findMitarbeiter);
 					break;
@@ -128,21 +114,13 @@ public class Main {
 			}
 			case 3: {
 				switch (input = Integer
-						.parseInt(inputValues("Was moechten sie tun ?\n" + "(1) Insert\n" + "(2) Delete\n"
+						.parseInt(userInput.inputValues("Was moechten sie tun ?\n" + "(1) Insert\n" + "(2) Delete\n"
 								+ "(3) Select All\n" + "(4) Select by ID\n" + "Bitte nur Zahlen eingeben: ", sc))) {
 				case 1: {
-					int id = Integer.parseInt(inputValues("Bitte ID eingeben: ", sc));
-					String vorname = inputValues("Bitte Vorname eingeben: ", sc);
-					String nachname = inputValues("Bitte Nachname eingeben: ", sc);
-					String strasse = inputValues("Bitte Strasse eingeben: ", sc);
-					String plz = inputValues("Bitte Plz eingeben: ", sc);
-					String ort = inputValues("Bitte Ort eingeben: ", sc);
-					String telefon = inputValues("Bitte Telefon eingeben: ", sc);
-					String email = inputValues("Bitte Email eingeben: ", sc);
-					String branche = inputValues("Bitte Branche eingeben: ", sc);
+					kunden = userInput.setKunden(sc);
 
-					daoKunden.insert(
-							kunden = new Kunden(id, vorname, nachname, strasse, plz, ort, telefon, email, branche));
+					daoKunden.insert(kunden);
+							
 					System.out.println();
 					System.out.println("Sie haben folgenden Daten hizugefuegt: " + kunden);
 
@@ -150,7 +128,7 @@ public class Main {
 				}
 				case 2: {
 					kunden.setKundenNr(
-							input = Integer.parseInt(inputValues("Sie möchte Kunden mit welche ID loeschen:...", sc)));
+							input = Integer.parseInt(userInput.inputValues("Sie möchte Kunden mit welche ID loeschen:...", sc)));
 					daoKunden.delete(kunden);
 					break;
 				}
@@ -161,7 +139,7 @@ public class Main {
 					break;
 				}
 				case 4: {
-					kunden.setKundenNr(input = Integer.parseInt(inputValues("Sie moechte Kunden mit ID:  ", sc)));
+					kunden.setKundenNr(input = Integer.parseInt(userInput.inputValues("Sie moechte Kunden mit ID:  ", sc)));
 					Kunden findKunden = daoKunden.selectById(kunden);
 					System.out.println(findKunden);
 					break;
@@ -182,14 +160,5 @@ public class Main {
 			}
 // bis hier
 		}
-
-	}
-
-	public static String inputValues(String text, Scanner sc) {
-
-		System.out.print(text);
-		String input = sc.nextLine();
-
-		return input;
 	}
 }
